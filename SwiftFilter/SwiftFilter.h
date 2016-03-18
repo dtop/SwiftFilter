535b854823e469bafd8b0fd395bfee829f68497e
Danilo Topalovic <danilo.topalovic@nerdsee.com>
2016-03-18 21:06:44 +0100

    Initial Commit

---
+++ SwiftFilter/SwiftFilter.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFilter.h
+//  SwiftFilter
+//
+//  Created by Danilo Topalovic on 18.03.16.
+//  Copyright © 2016 nerdsee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFilter.
+FOUNDATION_EXPORT double SwiftFilterVersionNumber;
+
+//! Project version string for SwiftFilter.
+FOUNDATION_EXPORT const unsigned char SwiftFilterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFilter/PublicHeader.h>
+
+
